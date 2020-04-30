efafd2fdffe20b2b492854f3e74104ccdd8b2b15
robarcand <robbyarcand@aol.com>
2020-04-30 19:59:39 -0400

    completed assignment

--- views/index.js
+++ views/index.js
@@ -4,5 +4,6 @@ const main = require("./main");
 const userList = require("./userList");
 const userPages = require("./userPages");
 const wikiPage = require("./wikiPage");
+const error = require('./error');
 
 module.exports = { addPage, editPage, error, main, userList, userPages, wikiPage };
